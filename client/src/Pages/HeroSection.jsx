e350d0468ee1607cfc9f6a353501497909dfd756
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-04 18:52:31 +0530

    feat:herosection

--- client/src/Pages/HeroSection.jsx
+++ client/src/Pages/HeroSection.jsx
@@ -1,53 +1,54 @@
+import { useRef, useEffect } from "react";
+import airbnd from "../assets/airbnd.png";
+import nasa from "../assets/nasa.png";
+import uber from "../assets/uber.png";
+import target from "../assets/target.png";
+import nyt from "../assets/nyt.png";
+import etsy from "../assets/etsy.png";
+
 const HeroSection = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = true;
+      videoRef.current.play().catch((error) => {
+        console.error("Error attempting to play video", error);
+      });
+    }
+  }, []);
+
   return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="text-xl btn btn-ghost">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle"
-            >
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="badge badge-sm indicator-item">8</span>
-              </div>
-            </div>
-            <div
-              tabIndex={0}
-              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-            >
-              <div className="card-body">
-                <span className="text-lg font-bold">8 Items</span>
-                <span className="text-info">Subtotal: $999</span>
-                <div className="card-actions">
-                  <button className="btn btn-primary btn-block">
-                    View cart
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="w-3/4 py-8 mx-auto text-center">
+      <h1 className="py-4 text-5xl font-bold">
+        {" "}
+        Made for People.{" "}
+        <span className="text-[#451C4A]  ">Built for productivity</span>
+      </h1>
+      <button className="font-semibold  btn px-10 h-14  text-white bg-[#562F5C] my-3 hover:bg-[#451c4a]">
+        {" "}
+        GET STARTED
+      </button>
+      <p className="py-2 mb-2">
+        {" "}
+        <span className="font-bold "> Slack if free to try</span> Slack if free
+        to try for as long as you like{" "}
+      </p>
+      <div className="flex items-center py-5 mb-4 justify-evenly">
+        <img className="w-20 " src={airbnd} alt="air bnb" />
+        <img className="w-16" src={nasa} alt=" nasa" />
+        <img className="w-16" src={uber} alt="uber" />
+        <img className="w-12" src={target} alt="target" />
+        <img className="w-36" src={nyt} alt="new york" />
+        <img className="w-16" src={etsy} alt="etsy" />
       </div>
-    </>
+      <video
+        autoPlay
+        ref={videoRef}
+        className="rounded-xl"
+        src="https://a.slack-edge.com/0cedc3b/marketing/img/homepage/true-prospects/hero-revamp/animation/hero@2x.IN.webm"
+      ></video>
+    </div>
   );
 };
 
