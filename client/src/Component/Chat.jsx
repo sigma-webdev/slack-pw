ab2220527be1e5a25a15bb2bedb34602f7cdf6da
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-10 15:41:43 +0530

    feat: chat profile section

--- client/src/Component/Chat.jsx
+++ client/src/Component/Chat.jsx
@@ -2,12 +2,16 @@ import { useRef, useState } from "react";
 import threadchat from "../assets/thread-message.svg";
 import searchImage from "../assets/magnifying-glass.svg";
 import userProfile from "../assets/slackbot.png";
+import arrowDown from "../assets/chevron-down.svg";
 
 const Chat = () => {
   const [width, setWidth] = useState(300); // Initial width of the sidebar
   const sidebarRef = useRef(null);
   const isDragging = useRef(false);
 
+  {
+    /* drag -- TODO: fix glitch drag */
+  }
   const startDrag = (e) => {
     isDragging.current = true;
     document.addEventListener("mousemove", handleMouseMove);
@@ -67,7 +71,7 @@ const Chat = () => {
         <div className="mt-2 bg-[#42424A] flex  items-start py-3 px-1 gap-2">
           <img src={userProfile} alt="profile avatar" width={25} />
           <div>
-            <h3> John doe </h3>
+            <h3> Mangesh Thakre </h3>
             <p className="overflow-hidden"> hello world john meet ...</p>
           </div>
         </div>
@@ -77,10 +81,56 @@ const Chat = () => {
         onMouseDown={startDrag}
       ></div>
 
-      {/* drag -- TODO: fix glitch drag */}
-      <div className="w-full mt-5 bg-white rounded-r-md">
-        {" "}
-        column column three{" "}
+      {/* chat message part */}
+      <div className="w-full mt-5 text-black bg-[#222529] rounded-r-md overflow-y-scroll scrollable-container">
+        {/* chat name section   */}
+
+        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+        <div className=" text-[#D1D2D3] p-2 border-b border-gray-500">
+          <button
+            onClick={() => document.getElementById("my_modal_3").showModal()}
+            className="flex items-center gap-2"
+          >
+            <img src={userProfile} alt="profile avatar" width={30} />
+            <h3 className="text-sm"> Mangesh Thakre </h3>
+            <img src={arrowDown} alt="arrow down" />
+          </button>
+        </div>
+
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <h3 className="text-lg font-bold">Hello!</h3>
+            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+          </div>
+        </dialog>
+
+        {/* chat profile name section */}
+        <div className="pt-40  text-[#D1D2D3]">
+          <div className="flex items-center gap-4 font-bold">
+            <img src={userProfile} alt="profile avatar" width={100} />
+            <h3 className="text-2xl"> Mangesh Thakre </h3>
+          </div>
+          <p className="mt-2 text-lg">
+            {" "}
+            This conversation is just between{" "}
+            <span className="text-[#2BA3C4]">@Mangesh</span> Thakre and you.
+            Check out their profile to learn more about them.
+          </p>
+          <button className="mt-3 btn btn-outline text-[#D1D2D3] hover:bg-[#d1d2d376]">
+            view Profile
+          </button>
+        </div>
+
+        {/* chat section */}
+        <div>
+          <p> chat section </p>
+        </div>
       </div>
     </section>
   );
