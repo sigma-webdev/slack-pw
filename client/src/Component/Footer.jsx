8b20c5f1409ddd86f57b99cfa1b575449fd8d8c4
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-10 21:59:55 +0530

    fix: footer visibility

--- client/src/Component/Footer.jsx
+++ client/src/Component/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <div className="bottom-0 flex justify-center w-full gap-3 py-4 ">
-      <a className=""> Privacy & Terms </a>
-      <a> Contact Us </a>
-      <a> Change region </a>
+    <div className="bottom-0 flex items-center justify-center w-full gap-3 py-5 mt-5 bg-white">
+      <a className="cursor-pointer"> Privacy & Terms </a>
+      <a className="cursor-pointer"> Contact Us </a>
+      <a className="cursor-pointer"> Change region </a>
     </div>
   );
 };
