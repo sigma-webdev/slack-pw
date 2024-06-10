f256c542f1a9b468b81627adb17cbcfa6690d4ad
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-10 22:38:41 +0530

    fix: nav search ui

--- client/src/Pages/NavBar.jsx
+++ client/src/Pages/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = ({ isAuth }) => {
       }`}
     >
       {/* left nav logo and items */}
-      <div className={`w-4/5 navbar-start  `}>
+      <div className={`w-3/6 navbar-start  `}>
         <Link to={"/"} className="pl-0 pr-5 btn btn-ghost">
           {isAuth ? (
             <img className="" src={slackLogoWhite} alt="slack logo" />
@@ -134,22 +134,32 @@ const NavBar = ({ isAuth }) => {
 
       {/* right nav search sign in and buttons */}
       <div className=" md:gap-2 navbar-end">
-        <button className=" btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
+        <div className="flex items-center px-3 py-1 rounded-md hover:border focus:border">
+          <input
+            type="text"
+            id="search"
+            className={`w-28 focus:outline-none ${
+              isAuth ? "bg-[#4A154b] text-white" : "bg-white text-black"
+            } font-normal`}
+          />
+          <label htmlFor="search">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </label>
+        </div>
+
         {!isAuth && (
           <Link to={"/signin"} className="hidden w-12 text-sm lg:block ">
             {" "}
@@ -170,7 +180,7 @@ const NavBar = ({ isAuth }) => {
           </a>
           {isAuth ? (
             <Link
-              to={"/chats"}
+              to={"/chat"}
               className=" align-center   btn bg-white font-semibold text-[#4A154B]  shadow-inner hover:bg-white hover:shadow-[#4a154b49] "
             >
               START CHATTING
